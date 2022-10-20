cc6ab9aecd25b84dbf6945673c48b0a1c7c64bc4
그램 <darkzhon@hanmail.net>
2022-10-21 00:27:28 +0900

    이미지 파일 올리기, PUBLIC_URL 사용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,5 +1,8 @@
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar, Row, Col  } from 'react-bootstrap';
+
 import './App.css';
+import bg from './img/bg.jpg';
+
 
 function App() {
   return (
@@ -9,11 +12,31 @@ function App() {
           <Navbar.Brand href="#home">삐에로쇼핑</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      <div className='main-bg' style={{backgroundImage:'url('+bg+')'}}></div>
+
+      <Container>
+        <Row>
+        <Col md={4}>
+            <img src={process.env.PUBLIC_URL + "/shoes1.jpg"} width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col md={4}>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col md={4}>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
