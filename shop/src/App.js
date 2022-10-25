8862081dccf638adcbedfd7ddcf2de8cb892878d
그램 <darkzhon@hanmail.net>
2022-10-26 02:10:47 +0900

    신발 정보를 저장하고 있는 data.js export 설정

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,16 @@
 import { Button, Container, Nav, Navbar, Row, Col  } from 'react-bootstrap';
-
 import './App.css';
 import bg from './img/bg.jpg';
 
+import React, { useState } from 'react';
+
+import shoesInfo from './data.js'
+
 
 function App() {
+
+  let [shoes, setShoes] = useState(shoesInfo);
+
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -17,23 +23,22 @@ function App() {
         </Container>
       </Navbar>
       <div className='main-bg' style={{backgroundImage:'url('+bg+')'}}></div>
-
       <Container>
         <Row>
         <Col md={4}>
             <img src={process.env.PUBLIC_URL + "/shoes1.jpg"} width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </Col>
           <Col md={4}>
             <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
           </Col>
           <Col md={4}>
             <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
           </Col>
         </Row>
       </Container>
