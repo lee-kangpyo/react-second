b1c9bd9e3465935ddfd887fb2719d48d4c98a69e
그램 <darkzhon@hanmail.net>
2022-12-21 23:20:34 +0900

    익스프레스 리액트 연동

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,7 +1,31 @@
-const exress = require('express')
+const express = require('express')
 const path = require('path')
-const app = exress()
+const app = express()
 
 app.listen(8080, function (){
     console.log("listening on 8080")
+})
+
+// 유저가 보낸 array/object 데이터 출력해보기 위해 필요
+app.use(express.json())
+// 다른 도메인 주소끼리 ajax 요청 주고 받을때 필요
+// npm install cors
+var cors = require("cors")
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+app.get("/", function(request, response){
+    request.sendFile(path.join(__dirname, 'client/build/index.html'))
+});
+
+// api 요청을 할수 있다.
+app.get("/product", function(request, response){
+    // 서버에서 데이터를 받아온후 아래와같이 전달
+    request.json({"name":"제품명"})
+});
+
+
+app.get("*", function(request, response){
+    request.sendFile(path.join(__dirname, 'client/build/index.html'))
 })
