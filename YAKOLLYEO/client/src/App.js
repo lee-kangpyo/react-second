26b7390c90b9219a09bd9827050953cedee33500
그램 <darkzhon@hanmail.net>
2023-04-05 01:12:15 +0900

    리액트에 프록시 설정

--- YAKOLLYEO/client/src/App.js
+++ YAKOLLYEO/client/src/App.js
@@ -16,18 +16,17 @@ function App() {
 
   useEffect(() => {
     // express 에서 값 받아오기
-    axios.get('http://localhost:8080/test/getData').then((res) => {
+    axios.get('/test/getData').then((res) => {
       setServerData(res.data.value)
     })
     // 약올려 db에서 값 받아보기
-    axios.get('http://localhost:8080/test/getTestYak?cstCl=PH').then((res) => {
-      setYakollyeoData(JSON.stringify(res.data.recordset))
+    axios.get('/test/getTestYak?cstCl=PH').then((res) => {
+      setYakollyeoData(JSON.stringify(res.data.recordset[0]))
     })
   }, [])
 
   return (
     <div className="App">
-      
       <div>리액트로 만든 html</div>
       <div>{serverData}</div>
       <div>{yakollyeoData}</div>
