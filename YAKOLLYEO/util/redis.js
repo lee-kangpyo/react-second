b8133b862ae23052404615f0080ac18bd7c2db0c
그램 <darkzhon@hanmail.net>
2023-04-13 18:53:23 +0900

    redis 레거시 모드 사용 예제

--- YAKOLLYEO/util/redis.js
+++ YAKOLLYEO/util/redis.js
@@ -7,11 +7,27 @@ const redisClient = redis.createClient({
         port: process.env.REDIS_PORT,
         host: process.env.REDIS_HOST,
       },
+      legacyMode: true,
 });
 
 
-redisClient.on('connect', () => console.log('Connected to Redis!'));
+//redisClient.on('connect', () => console.log('Connected to Redis!'));
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on('connect', () => {
+    console.log('Connected to Redis!!!');
+    
+    redisClient.set('hello', 'world');
+    redisClient.get('key', (err, value) => {
+        console.log(value); // 123
+    })
+    redisClient.get('hello', (err, value) => {
+        console.log(value); // 123
+    })
+  
+    
+  });
+
 redisClient.connect();
+
 
 module.exports = redisClient;
